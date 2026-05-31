8edd8730ba3d29c67c92ff3380b356ef3341666b
Jakub Szydełko <kubarroo248@interia.pl>
2026-05-31 16:33:34 +0200

    OpenGL rendering replaced with SDL3 implementation

--- App/main.cpp
+++ App/main.cpp
@@ -2,70 +2,104 @@
 #include <string>
 #include <omp.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/logger.hpp>
 
 #define SDL_MAIN_HANDLED
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+
 #include <imgui.h>
 #include <imgui_impl_sdl3.h>
-#include <imgui_impl_opengl3.h>
-
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <SDL3/SDL_opengles2.h>
-#else
-#include <SDL3/SDL_opengl.h>
-#endif
+#include <imgui_impl_sdlrenderer3.h>
 
 struct AppContext {
-    SDL_Window* window;
-    int window_w;
-    int window_h;
-    int display_w;
-    int display_h;
-    std::string dropped_file_path = "Brak upuszczonego pliku. Przeciągnij plik tutaj!";
-    GLuint background_texture = 0;
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    int window_w = 0;
+    int window_h = 0;
+
+    std::string dropped_file_path =
+        "Brak upuszczonego pliku. Przeciągnij plik tutaj!";
+
+    SDL_Texture* background_texture = nullptr;
 };
 
-GLuint MatToTexture(const cv::Mat& mat) {
-    if (mat.empty()) return 0;
-
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+SDL_Texture* MatToTexture(SDL_Renderer* renderer, const cv::Mat& mat) {
+    if (!renderer || mat.empty()) {
+        return nullptr;
+    }
 
     cv::Mat rgbMat;
-    cv::cvtColor(mat, rgbMat, cv::COLOR_BGR2RGB);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, rgbMat.cols, rgbMat.rows, 0, GL_RGB, GL_UNSIGNED_BYTE, rgbMat.data);
+    if (mat.channels() == 3) {
+        cv::cvtColor(mat, rgbMat, cv::COLOR_BGR2RGB);
+    } else if (mat.channels() == 4) {
+        cv::cvtColor(mat, rgbMat, cv::COLOR_BGRA2RGBA);
+    } else if (mat.channels() == 1) {
+        cv::cvtColor(mat, rgbMat, cv::COLOR_GRAY2RGB);
+    } else {
+        return nullptr;
+    }
 
-    glBindTexture(GL_TEXTURE_2D, 0);
-    return textureID;
+    SDL_PixelFormat format =
+        rgbMat.channels() == 4 ? SDL_PIXELFORMAT_RGBA32 : SDL_PIXELFORMAT_RGB24;
+
+    SDL_Texture* texture = SDL_CreateTexture(
+        renderer,
+        format,
+        SDL_TEXTUREACCESS_STATIC,
+        rgbMat.cols,
+        rgbMat.rows
+    );
+
+    if (!texture) {
+        std::cerr << "Nie udało się utworzyć tekstury SDL: "
+                  << SDL_GetError() << std::endl;
+        return nullptr;
+    }
+
+    SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_LINEAR);
+
+    const int pitch = static_cast<int>(rgbMat.step);
+    if (!SDL_UpdateTexture(texture, nullptr, rgbMat.data, pitch)) {
+        std::cerr << "Nie udało się zaktualizować tekstury SDL: "
+                  << SDL_GetError() << std::endl;
+        SDL_DestroyTexture(texture);
+        return nullptr;
+    }
+
+    return texture;
+}
+
+void DestroyBackgroundTexture(AppContext& ctx) {
+    if (ctx.background_texture) {
+        SDL_DestroyTexture(ctx.background_texture);
+        ctx.background_texture = nullptr;
+    }
 }
 
 void RenderFrame(AppContext& ctx) {
     SDL_GetWindowSize(ctx.window, &ctx.window_w, &ctx.window_h);
-    SDL_GetWindowSizeInPixels(ctx.window, &ctx.display_w, &ctx.display_h);
 
-    ImGuiIO& io = ImGui::GetIO();
-    if (ctx.window_w > 0 && ctx.window_h > 0) {
-        io.DisplaySize = ImVec2((float)ctx.window_w, (float)ctx.window_h);
-        io.DisplayFramebufferScale = ImVec2((float)ctx.display_w / ctx.window_w, (float)ctx.display_h / ctx.window_h);
-    }
-
-    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDLRenderer3_NewFrame();
     ImGui_ImplSDL3_NewFrame();
     ImGui::NewFrame();
 
-    if (ctx.background_texture != 0) {
+    if (ctx.background_texture) {
         ImDrawList* background_draw_list = ImGui::GetBackgroundDrawList();
+
         ImVec2 p_min = ImVec2(0.0f, 0.0f);
-        ImVec2 p_max = ImVec2((float)ctx.window_w, (float)ctx.window_h);
-        background_draw_list->AddImage((ImTextureID)(intptr_t)ctx.background_texture, p_min, p_max);
+        ImVec2 p_max = ImVec2(
+            static_cast<float>(ctx.window_w),
+            static_cast<float>(ctx.window_h)
+        );
+
+        background_draw_list->AddImage(
+        (ImTextureID)(intptr_t)ctx.background_texture,
+                p_min,
+                p_max
+            );
     }
 
     ImGui::Begin("Panel kontrolny");
@@ -76,122 +110,119 @@ void RenderFrame(AppContext& ctx) {
     ImGui::Begin("Obsługa plików");
     ImGui::TextWrapped("Ścieżka do ostatnio upuszczonego pliku:");
     ImGui::Separator();
-    ImGui::TextColored(ImVec4(0.2f, 0.8f, 0.2f, 1.0f), "%s", ctx.dropped_file_path.c_str());
+    ImGui::TextColored(
+        ImVec4(0.2f, 0.8f, 0.2f, 1.0f),
+        "%s",
+        ctx.dropped_file_path.c_str()
+    );
     ImGui::End();
 
     ImGui::Render();
-    glViewport(0, 0, ctx.display_w, ctx.display_h);
 
-    glClearColor(0.15f, 0.15f, 0.15f, 1.00f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_SetRenderDrawColor(ctx.renderer, 38, 38, 38, 255);
+    SDL_RenderClear(ctx.renderer);
 
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    ImGui_ImplSDLRenderer3_RenderDrawData(
+        ImGui::GetDrawData(),
+        ctx.renderer
+    );
 
-    SDL_GL_SwapWindow(ctx.window);
-}
-
-bool SDLCALL WindowEventWatcher(void* userdata, SDL_Event* event) {
-    AppContext* ctx = static_cast<AppContext*>(userdata);
-    SDL_WindowID window_id = SDL_GetWindowID(ctx->window);
-
-    if (event->window.windowID == window_id) {
-        if (event->type == SDL_EVENT_WINDOW_EXPOSED ||
-            event->type == SDL_EVENT_WINDOW_RESIZED ||
-            event->type == SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED) {
-
-            RenderFrame(*ctx);
-        }
-    }
-    return true;
+    SDL_RenderPresent(ctx.renderer);
 }
 
 int main(int argc, char* argv[]) {
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
     cv::Mat image = cv::Mat::zeros(300, 300, CV_8UC3);
     cv::circle(image, cv::Point(150, 150), 100, cv::Scalar(0, 255, 0), -1);
 
     SDL_SetMainReady();
+
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
-        std::cerr << "Blad SDL_Init: " << SDL_GetError() << std::endl;
+        std::cerr << "Błąd SDL_Init: " << SDL_GetError() << std::endl;
         return -1;
     }
 
 #pragma omp parallel
     {
 #pragma omp master
-        std::cout << "Liczba dostepnych watkow OpenMP: " << omp_get_num_threads() << std::endl;
+        std::cout << "Liczba dostępnych wątków OpenMP: "
+                  << omp_get_num_threads() << std::endl;
     }
 
-    const char* glsl_version = "#version 130";
-#if defined(__APPLE__)
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    glsl_version = "#version 150";
-#else
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#endif
+    SDL_Window* window = SDL_CreateWindow(
+        "Projekt C++ (SDL3 + ImGui + OpenCV + OpenMP)",
+        1280,
+        720,
+        SDL_WINDOW_RESIZABLE
+    );
 
-    SDL_Window* window = SDL_CreateWindow("Projekt C++ (SDL3 + ImGui + OpenCV + OpenMP)", 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     if (!window) {
-        std::cerr << "Nie udalo sie utworzyc okna: " << SDL_GetError() << std::endl;
+        std::cerr << "Nie udało się utworzyć okna: "
+                  << SDL_GetError() << std::endl;
         SDL_Quit();
         return -1;
     }
 
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_MakeCurrent(window, gl_context);
-#if defined(__APPLE__)
-    SDL_GL_SetSwapInterval(0);
-#else
-    SDL_GL_SetSwapInterval(1);
-#endif
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    if (!renderer) {
+        std::cerr << "Nie udało się utworzyć renderera SDL: "
+                  << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_SetRenderVSync(renderer, 1);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+
     ImGui::StyleColorsDark();
 
-    ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
-    ImGui_ImplOpenGL3_Init(glsl_version);
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
 
     AppContext ctx{};
     ctx.window = window;
+    ctx.renderer = renderer;
     SDL_GetWindowSize(window, &ctx.window_w, &ctx.window_h);
-    SDL_GetWindowSizeInPixels(window, &ctx.display_w, &ctx.display_h);
-    ctx.background_texture = MatToTexture(image);
+
+    ctx.background_texture = MatToTexture(renderer, image);
+
     const SDL_WindowID window_id = SDL_GetWindowID(window);
 
-    SDL_AddEventWatch(WindowEventWatcher, &ctx);
-
     bool running = true;
+
     while (running) {
         SDL_Event event;
+
         while (SDL_PollEvent(&event)) {
             ImGui_ImplSDL3_ProcessEvent(&event);
 
-            if (event.type == SDL_EVENT_QUIT)
+            if (event.type == SDL_EVENT_QUIT) {
                 running = false;
+            }
 
-            if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == window_id)
+            if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
+                event.window.windowID == window_id) {
                 running = false;
+            }
 
             if (event.type == SDL_EVENT_DROP_FILE) {
                 if (event.drop.data != nullptr) {
                     ctx.dropped_file_path = std::string(event.drop.data);
 
                     cv::Mat new_image = cv::imread(ctx.dropped_file_path);
-                    if (!new_image.empty()) {
-                        if (ctx.background_texture != 0) {
-                            glDeleteTextures(1, &ctx.background_texture);
-                            ctx.background_texture = 0;
-                        }
-                        ctx.background_texture = MatToTexture(new_image);
-                    }
 
+                    if (!new_image.empty()) {
+                        DestroyBackgroundTexture(ctx);
+                        ctx.background_texture =
+                            MatToTexture(renderer, new_image);
+                    }
                 }
             }
         }
@@ -199,18 +230,13 @@ int main(int argc, char* argv[]) {
         RenderFrame(ctx);
     }
 
-    SDL_RemoveEventWatch(WindowEventWatcher, &ctx);
+    DestroyBackgroundTexture(ctx);
 
-    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDLRenderer3_Shutdown();
     ImGui_ImplSDL3_Shutdown();
     ImGui::DestroyContext();
 
-    if (ctx.background_texture != 0) {
-        glDeleteTextures(1, &ctx.background_texture);
-        ctx.background_texture = 0;
-    }
-
-    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
